8c344e055b8c5cd69fe720c025984e568ce60175
Jack Rydzik <p23jrydzik@fairfieldprep.org>
2022-11-03 10:56:26 -0400

    finished lab 6-1

---
+++ lab_6-1.py
@@ -0,0 +1,19 @@
+# author: JHR 11/3/22
+
+# defining list 
+my_arr = [1, 2, 3, 4, 5]
+
+# printing the 2nd index of the list
+print (my_arr[2])
+
+# printing the length of the list
+print (len(my_arr))
+
+# printing the list times 3
+print (my_arr * 3)
+
+# defining string variable
+s = "string"
+
+# coverting new s variable to list and printing it
+print (list(s))
