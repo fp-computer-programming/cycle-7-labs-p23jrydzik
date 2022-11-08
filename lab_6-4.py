b4f47601f33b5648cf54d6aa5a052b7f1823e6b7
Jack Rydzik <p23jrydzik@fairfieldprep.org>
2022-11-08 13:29:06 -0500

    finished 6-4

---
+++ lab_6-4.py
@@ -0,0 +1,20 @@
+# author: JHR 11/8/22
+
+# creating my list
+listuno = ["math", "english", "theology"]
+
+# adding 4th entry
+listuno.append ("programming")
+
+# printing the 2nd index in list
+print (listuno[1])
+
+# sorting list and printing it
+listuno.sort()
+print (listuno)
+
+# copying list
+listdos = listuno.copy()
+
+# reversing copy
+listdos.sort(reverse=True)
